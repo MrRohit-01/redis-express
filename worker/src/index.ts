f65e6c4f2868fb1c212972cd49809f682e1184f4
Rohit Kumar Barada <rohitbarada@gmail.com>
2025-09-16 02:20:15 +0530

    redis pubsub and messaging queue

---
+++ worker/src/index.ts
@@ -0,0 +1,47 @@
+import { createClient } from "redis";
+const client = createClient();
+const sub = client.duplicate();
+
+
+async function processSubmission(submission: string) {
+    const { problemId, code, language,jobId } = JSON.parse(submission);
+
+    console.log(`Processing submission for problemId ${problemId}...`);
+    console.log(`Code: ${code}`);
+    console.log(`Language: ${language}`);
+    console.log(`jobId: ${jobId}`);
+
+    
+    // Here you would add your actual processing logic
+    
+    // Simulate processing delay
+    await new Promise(resolve => setTimeout(resolve, 10000));
+    console.log(`Finished processing submission for problemId ${problemId}.`);
+    sub.publish(`result${jobId}`,`Done the work ${jobId}`);
+}
+
+async function startWorker() {
+    
+    try {
+        await client.connect();
+        await sub.connect();
+        console.log("Worker connected to Redis.");
+
+        // Main loop
+        while (true) {
+            try {
+                const submission = await client.brPop("problems", 0);
+                // @ts-ignore
+                await processSubmission(submission.element);
+            } catch (error) {
+                console.error("Error processing submission:", error);
+                // Implement your error handling logic here. For example, you might want to push
+                // the submission back onto the queue or log the error to a file.
+            }
+        }
+    } catch (error) {
+        console.error("Failed to connect to Redis", error);
+    }
+}
+
+startWorker();
