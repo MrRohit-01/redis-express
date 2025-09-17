83b0322de533f051851ea825589b9c6e1a376c59
Rohit Kumar Barada <rohitbarada@gmail.com>
2025-09-18 00:02:29 +0530

    update

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -24,7 +24,7 @@ async function startWorker() {
     
     try {
         await client.connect();
-        await sub.connect();
+        await sub.connect();    
         console.log("Worker connected to Redis.");
 
         // Main loop
